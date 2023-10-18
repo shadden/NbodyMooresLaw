fd5da9dca14b2e9825b881330af66c5f80e26b34
Sam Hadden <shadden1107@gmail.com>
2023-10-17 21:10:27 -0400

    pretty-fication

--- NbodyMooresLaw.py
+++ NbodyMooresLaw.py
@@ -19,19 +19,20 @@ MIN = 60
 HR = 60 * MIN
 DAY = 24 * HR
 MONTH = 30 * DAY
+S=8
 papers = {
-    1950:((40/365)/(2*MIN),0,"Ekert '52",0,"EBC52"),
-    1965:((1500)/(HR),0,"Cohen & Hubbard '65",0,"CH65"),
-    1978:((40/365)/(10),0,"TRS-80",0,"TRS-80"),
-    1984.1:(5e6/(4*HR),0,"Kinoshita & Nakai'84",1,"KN84"),
-    1986:(60*1e8/(365*DAY),0,"Applegate+ '86",1,"A+86"),
-    1991.1:(3e6/(2*MONTH),1,"Quinn+ '91",0,"QDT91"),
-    1991:(1e9/(14*DAY),0,"Wisdom & Holman '91",0,"WH91"),
-    2008:(20e9 / (6*MONTH),1,"Batygin & Laughlin '08",0,"BL08"),
-    2009:(5e9 / (2500*HR),1,"Laskar & Gastineau '09",0,"LG09"),
-    2020:(5e9*96 / (6*12*MONTH),1,"Brown & Rein '20",0,"BR20"),
-    2023:(2 * 2750 * 5e9 / (2.5e6*HR) ,1,"Abbot+ '23",0,"A+23"),
-    2023.1:(5e9 / (DAY) ,1,"Javaheri+ '23",0,"JRT23"),
+    1952:((40/365)/(2*MIN),0,"Ekert '52",0,"EBC52",(S,S)),
+    1965:((1500)/(HR),0,"Cohen & Hubbard '65",0,"CH65",(S,S)),
+    1978:((40/365)/(10),0,"TRS-80",0,"TRS-80",(S,S)),
+    1984.1:(5e6/(4*HR),0,"Kinoshita & Nakai'84",1,"KN84",(-5*S,S)),
+    1986:(60*1e8/(365*DAY),0,"Applegate+ '86",1,"A+86",(S,S)),
+    1991.1:(3e6/(2*MONTH),1,"Quinn+ '91",0,"QDT91",(S,S)),
+    1991:(1e9/(14*DAY),0,"Wisdom & Holman '91",0,"WH91",(S,S)),
+    2008:(20e9 / (6*MONTH),1,"Batygin & Laughlin '08",0,"BL08",(S,S)),
+    2009:(5e9 / (2500*HR),1,"Laskar & Gastineau '09",0,"LG09",(S,S)),
+    2020:(5e9*96 / (6*12*MONTH),1,"Brown & Rein '20",0,"BR20",(-5*S,S)),
+    2023:(2 * 2750 * 5e9 / (2.5e6*HR) ,1,"Abbot+ '23",0,"A+23",(S,S)),
+    2023.1:(5e9 / (DAY) ,1,"Javaheri+ '23",0,"JRT23",(S,S)),
 }
 
 # CPU clock speed data
@@ -52,40 +53,67 @@ plt.tick_params(labelsize=16,size=8,direction='in')
 plt.tick_params(size=6,which='minor',direction='in')
 TO_MYR_PER_MONTH = MONTH/1e6
 for year,data in papers.items():
-    rate,inner,label,hardware,shortname=data
+    rate,inner,label,hardware,shortname,txtoffset=data
+    if year==1952:
+        lbl = "Simulation Efficiency [Myr/CPU month]"
+    else:
+        lbl=None
     if inner:
         x,y=year,TO_MYR_PER_MONTH * rate
-        if year==2023:
-            plt.scatter(x,y,color='k',zorder=99,marker='*',s=150,label='Simulation Efficiency [Myr/CPU month]')
-        else:
-            plt.scatter(x,y,color='k',zorder=99,marker='*',s=150)
-        plt.text(x+0.2,y*1.25,shortname,ha='center',fontsize=12)
+        ax.scatter(x,y,marker='*',s=250,zorder=99,c='k',label=lbl)
+        #plt.text(x,y,shortname,zorder=99,fontsize=12,backgroundcolor='black',color='white')
+        ax.annotate(shortname,
+                    xy=(x,y),
+                    xytext=txtoffset,
+                    textcoords='offset points',
+                    color='black',
+                    weight='bold',
+                    bbox=dict(boxstyle='round,pad=0.2', fc='yellow', alpha=1)
+                )
     else:
         x,y = year,TO_MYR_PER_MONTH * rate / OUTER_TO_INNER_RESCALE 
-        plt.scatter(x,y,color='k',marker='*',s=150)
-        plt.text(x+0.2,y*1.25,shortname,ha='center',zorder=99,fontsize=12)
+        #plt.text(x,y,shortname,ha='center',zorder=99,fontsize=12,backgroundcolor='black',color='white')
+        ax.scatter(x,y,marker='*',s=250,zorder=99,c='k',label=lbl)
+        ax.annotate(
+            shortname,
+            xy=(x,y),
+            xytext=txtoffset, 
+            textcoords='offset points',
+            color='black',
+            weight='bold',
+            bbox=dict(boxstyle='round,pad=0.2', fc='yellow', alpha=1)
+            )
+    print(label, np.log10(y))
 # known planets
 ax.plot(
     np.concatenate(([1940],np.sort(disc_years))),
-    np.arange(8,1+8+len(disc_years))
-    ,'k-',
-    lw=3,
+    np.arange(8,1+8+len(disc_years)),
+    ls='-',
+    color='orange',
+    lw=6,
     label='Known Planets'
 )
 # CPU clock speed
-ax.plot(yr,freq,'b.',ms=3,zorder=0,label='CPU clock rates [MHz]')
+ax.plot(yr,freq,'bs',ms=8,zorder=0,label='CPU clock rates [MHz]')
+#set_xticks([20, 200, 500])
 
 # Cosmetics
 plt.yscale('log')
-plt.ylabel("$N$",fontsize=16)
+ax.set_ylim(10**(-4.5),10**(5.5))
+ax.set_yticks(10.**np.arange(-4,6))
+minor_tx = np.array([np.arange(2,10) * pow10 for pow10 in 10.**np.arange(-4,6)])
+minor_tx = minor_tx.reshape(-1)
+ax.set_yticks(minor_tx,minor=True)
+plt.ylabel("$N$",fontsize=18)
 plt.xlim(1945,2035)
-plt.xlabel("Year",fontsize=16)
-plt.tick_params(labelsize=16,size=8,direction='in')
+plt.xlabel("Year",fontsize=18)
+plt.tick_params(labelsize=18,size=8,direction='in')
 plt.tick_params(size=6,which='minor',direction='in')
-plt.title("$N$-body simulations, computer efficiency, and planet discoveries",fontsize=16)
-plt.legend(loc='lower right',fontsize=12)
+plt.title("$N$-body simulations, CPU efficiency, and planet discoveries",fontsize=16)
+plt.legend(loc='lower right',fontsize=16)
 plt.tight_layout()
 outfile = "/Users/shadden/github_io_website/shadden.github.io/assets/images/Nbody-Moores-Law.png"
 plt.savefig(outfile)
 outfile="/Users/shadden/DropboxPersonal/Apps/Overleaf/UCSD_research_statement/Nbody-Moores-Law.pdf"
 plt.savefig(outfile)
+plt.show()
