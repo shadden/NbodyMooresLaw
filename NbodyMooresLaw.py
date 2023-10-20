f217de418a52b935a640b004952d738fe3ba4b71
Sam Hadden <shadden1107@gmail.com>
2023-10-20 11:01:03 -0400

    new point

--- NbodyMooresLaw.py
+++ NbodyMooresLaw.py
@@ -13,7 +13,7 @@ tab = service.search(query)
 hosts = set(tab.getcolumn('hostname').data)
 astro_table = tab.to_table()
 disc_years=astro_table['disc_year'].data.data
-
+MERC_PER_DIVIDED_BY_MOON_PER = 1/0.31
 # N-body simulation data
 MIN = 60
 HR = 60 * MIN
@@ -24,6 +24,7 @@ papers = {
     1952:((40/365)/(2*MIN),0,"Ekert '52",0,"EBC52",(S,S)),
     1965:((1500)/(HR),0,"Cohen & Hubbard '65",0,"CH65",(S,S)),
     1978:((40/365)/(10),0,"TRS-80",0,"TRS-80",(S,S)),
+    1983:((MERC_PER_DIVIDED_BY_MOON_PER)*((10/9)**2)*(3002 - 1411)/(9*DAY),1,"Newhall, Standish, and Williams (1983)",0,"NSW83",(S,S)),
     1984.1:(5e6/(4*HR),0,"Kinoshita & Nakai'84",1,"KN84",(-5*S,S)),
     1986:(60*1e8/(365*DAY),0,"Applegate+ '86",1,"A+86",(S,S)),
     1991.1:(3e6/(2*MONTH),1,"Quinn+ '91",0,"QDT91",(S,S)),
@@ -43,7 +44,7 @@ yr,freq=freq_data.T
 
 # Time re-scaling for simulations of outer SS only
 #   -- Note- should probably include factor of something like (8/4)**2 to be more fair
-OUTER_TO_INNER_RESCALE = (5.2/0.387)**(1.5)
+OUTER_TO_INNER_RESCALE = (5.2/0.387)**(1.5) * (9/5)**2
 
 
 # the plot
@@ -55,7 +56,7 @@ TO_MYR_PER_MONTH = MONTH/1e6
 for year,data in papers.items():
     rate,inner,label,hardware,shortname,txtoffset=data
     if year==1952:
-        lbl = "Simulation Efficiency [Myr/CPU month]"
+        lbl = "Simulation Efficiency\n[Myr/CPU month]"
     else:
         lbl=None
     if inner:
@@ -99,9 +100,9 @@ ax.plot(yr,freq,'bs',ms=8,zorder=0,label='CPU clock rates [MHz]')
 
 # Cosmetics
 plt.yscale('log')
-ax.set_ylim(10**(-4.5),10**(5.5))
-ax.set_yticks(10.**np.arange(-4,6))
-minor_tx = np.array([np.arange(2,10) * pow10 for pow10 in 10.**np.arange(-4,6)])
+ax.set_ylim(9e-6,10**(5.5))
+ax.set_yticks(10.**np.arange(-5,6))
+minor_tx = np.array([np.arange(2,10) * pow10 for pow10 in 10.**np.arange(-5,6)])
 minor_tx = minor_tx.reshape(-1)
 ax.set_yticks(minor_tx,minor=True)
 plt.ylabel("$N$",fontsize=18)
